ce31168369b8fcc33af927af4d2c57ba2eb05733
bal7a <singularity717@gmail.com>
2023-06-14 12:19:40 +0300

    added delete, update and listing

--- README.md
+++ README.md
@@ -9,7 +9,6 @@ This script is run manually to scrape new releases from different manga sites an
 It's a json file stores the manga related _info_ that I need to keep track of what needs to be scraped. These _info_ are
 
 ```
-"website": <website name>,
 "manga_url": <url that has new manga releases>,
 "name": <manga name>,
 "url": <latest manga url>
@@ -58,8 +57,7 @@ Nothing new (-_-)
 Add new manga
 
 ```
-$ manga_checker -n
-website: mangakakalot
+$ manga_checker -a
 manga_url: https://mangakakalot.com/read-aq1cl158504930275
 name: Kings' Viking
 url: https://mangakakalot.com/chapter/kings_viking/chapter_80
@@ -68,7 +66,7 @@ url: https://mangakakalot.com/chapter/kings_viking/chapter_80
 List manga alphabetically
 
 ```bash
-$ manga_checker -l
+$ manga_checker -la
 ```
 
 Count manga
--- manga_checker.py
+++ manga_checker.py
@@ -1,22 +1,21 @@
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs4
 import json
 import argparse
-import sys
+import pprint
+import re
 
-# TODO: This is breaks if I change the files position
-if sys.platform == "win32":
-    JSON_FILE = "G:\\Baka\\Repos\\my_projects\\manga_checker\\site_obj.json"
-else:
-    JSON_FILE = "/mnt/g/Baka/Repos/my_projects/manga_checker/site_obj.json"
+JSON_FILE = "./site_obj.json"
 
 MIN_MANGA_PANEL = 3
+
+pp = pprint.PrettyPrinter(indent=4)
 
 
 def check_for_updates(file):
     data = json.load(file)
-    for _, el in enumerate(data["sites"]):
-        website = el["website"]
+    for el in data:
+        idx = el["idx"]
         manga_url = el["manga_url"]
         url = el["url"]
         name = el["name"]
@@ -25,24 +24,35 @@ def check_for_updates(file):
         try:
             r = requests.get(manga_url)
             text = r.text
-            soup = BeautifulSoup(text, "lxml")
+            soup = bs4(text, "lxml")
 
             if r.ok:
                 last_entry_url = soup.select(css_selector)[0]["href"]
                 if not last_entry_url.startswith("https"):
-                    last_entry_url = "https://" + website + ".com/" + last_entry_url
+                    last_entry_url = (
+                        re.match("(https*:\/\/.*?)\/", url).group(1) + last_entry_url
+                    )
                 if url == last_entry_url:
-                    print("Nothing new (-_-)")
+                    print(f"{idx}- Nothing new (-_-)")
                 else:
                     r = requests.get(last_entry_url)
-                    soup = BeautifulSoup(r.text, "lxml")
+                    soup = bs4(r.text, "lxml")
                     if len(soup.find_all("img")) > MIN_MANGA_PANEL:
                         print(
-                            "Manga Update Found: \t\t" + name + "\t\t" + last_entry_url
+                            f"{idx}- Manga Update Found: \t\t"
+                            + name
+                            + "\t\t"
+                            + last_entry_url
                         )
-                        url = last_entry_url
+                        el["url"] = last_entry_url
+                    else:
+                        print(f"{idx}- Nothing new (-_-)")
         except requests.exceptions.RequestException as e:
             print(e)
+        except IndexError as e:
+            print(e)
+            print(url)
+            print()
         except KeyboardInterrupt:
             exit(0)
     return data
@@ -52,13 +62,20 @@ def add_manga():
     try:
         with open(JSON_FILE) as f:
             data = json.load(f)
-            w = input("website: ")
             m = input("manga_url: ")
             n = input("name: ")
             u = input("url: ")
-            if w and m and n and u:
-                data["sites"].append(
-                    {"website": w, "manga_url": m, "name": n, "url": u}
+            c = input("css selector: ")
+            if m and n and u:
+                idx = len(data)
+                data.append(
+                    {
+                        "idx": idx,
+                        "manga_url": m,
+                        "name": n,
+                        "url": u,
+                        "css_selector": c,
+                    }
                 )
                 print("Saved")
             else:
@@ -70,6 +87,46 @@ def add_manga():
     return data
 
 
+def delete_manga():
+    try:
+        list_manga(sort=False)
+        index = input("index: ")
+        with open(JSON_FILE) as f:
+            data = json.load(f)
+            del data[int(index)]
+            f.close()
+    except KeyboardInterrupt:
+        exit(0)
+
+    return data
+
+
+def list_manga(sort=False):
+    with open(JSON_FILE) as f:
+        data = json.load(f)
+        names = []
+        urls = {}
+        longest_len = 0
+        i = 0
+
+        for _, el in enumerate(data):
+            names.append(el["name"])
+            urls[el["name"]] = el["manga_url"]
+            longest_len = max(longest_len, len(el["name"]))
+
+        if sort:
+            for name in sorted(names):
+                print(str(i) + "- " + name.ljust(longest_len + 5), urls[name])
+                i += 1
+        else:
+            for name in names:
+                print(str(i) + "- " + name.ljust(longest_len + 5), urls[name])
+                i += 1
+
+        print()
+        f.close()
+
+
 def update_json(data):
     with open(JSON_FILE, "w") as f:
         json.dump(data, f, indent=4)
@@ -78,39 +135,44 @@ def update_json(data):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-n", "--new_manga", help="add new manga", action="store_true")
+    parser.add_argument("-a", "--add_manga", help="add new manga", action="store_true")
+    parser.add_argument(
+        "-d", "--delete_manga", help="delete existing manga", action="store_true"
+    )
     parser.add_argument("-c", "--count", help="how many manga", action="store_true")
     parser.add_argument("-l", "--list", help="list manga_sites", action="store_true")
+    parser.add_argument(
+        "-la",
+        "--list-alphabetically",
+        help="list manga_sites alphabetically",
+        action="store_true",
+    )
     args = parser.parse_args()
 
-    if args.new_manga:
+    if args.add_manga:
         data = add_manga()
+        update_json(data)
+        exit(0)
+
+    if args.delete_manga:
+        data = delete_manga()
         update_json(data)
         exit(0)
 
     if args.count:
         with open(JSON_FILE) as f:
             data = json.load(f)
-            print(len(data["sites"]))
+            print(len(data))
             f.close()
             exit(0)
 
     if args.list:
-        with open(JSON_FILE) as f:
-            data = json.load(f)
-            names = []
-            urls = {}
-            longest_len = 0
-            for _, el in enumerate(data["sites"]):
-                names.append(el["name"])
-                urls[el["name"]] = el["manga_url"]
-                longest_len = max(longest_len, len(el["name"]))
+        list_manga(sort=False)
+        exit(0)
 
-            for name in sorted(names):
-                print(name.ljust(longest_len + 5), urls[name])
-
-            f.close()
-            exit(0)
+    if args.list_alphabetically:
+        list_manga(sort=True)
+        exit(0)
 
     with open(JSON_FILE) as f:
         data = check_for_updates(f)
